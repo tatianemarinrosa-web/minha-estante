7f88fe99b9c158bd392e2cef089943be3e1876c2
tatianemarinrosa-web <tatianemarinrosa@gmail.com>
2026-08-10 15:17:08 -0300

    Adiciona suporte offline

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "minha-estante-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
