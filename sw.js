8439d09520893bdf213deff58e12bb05281f2ae4
tatianemarinrosa-web <tatianemarinrosa@gmail.com>
2026-08-15 23:02:41 -0300

    Atualiza cache para versão 7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "minha-estante-v6";
+const CACHE = "minha-estante-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
