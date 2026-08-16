19282d4c5f781ba64f6cc6bd306e34e20ff8a0da
tatianemarinrosa-web <tatianemarinrosa@gmail.com>
2026-08-16 12:05:09 -0300

    Atualiza cache e recursos para versão instalável

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "minha-estante-v7";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+const CACHE = "minha-estante-v8";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener("fetch", event => {
