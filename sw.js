8f5d84b822d6379bafc707c295ef3454d89d56a8
tatianemarinrosa-web <tatianemarinrosa@gmail.com>
2026-08-17 07:57:04 -0300

    Atualiza cache para versão mobile 9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "minha-estante-v8";
+const CACHE = "minha-estante-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
